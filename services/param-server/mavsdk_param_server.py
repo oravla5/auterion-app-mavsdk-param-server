3d21e4a88f324a0f26a03fb030d3dc64f453a407
oravla5 <alvaro@auterion.com>
2024-04-25 13:50:58 +0200

    Added some extra print statements

--- services/param-server/mavsdk_param_server.py
+++ services/param-server/mavsdk_param_server.py
@@ -12,8 +12,10 @@ async def main():
     await skynode.connect(system_address="tcp://172.17.0.1:5790")
     print("Connected to vehicle")
     
-    await skynode.param_server.provide_param_int("FANCY_PARAM_123", 45)
+    await skynode.param_server.provide_param_int("FANCY_VAL_12", 45)
+    print("Provided parameter to server")
 
 if __name__ == "__main__":
     print("Starting mavsdk_param_server.py")
     asyncio.run(main())
+    print("Completed custom parameter upload")
